1d4088b2151e92a4afd3a21ac269956244bc7e75
mark <njugunamark310@gmail.com>
2026-08-05 19:50:37 +0300

    improved contact page

--- script.js
+++ script.js
@@ -200,6 +200,7 @@ if (savedMenuItems) {
 
 }
 // contact page
+const contactForm = document.querySelector(".contact-form");
 if (contactForm) {
     contactForm.addEventListener("submit", function (event) {
         event.preventDefault();
